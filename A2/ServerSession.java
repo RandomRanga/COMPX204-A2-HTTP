ccfff07bb3b98718fadcaf7ea7c257bf3f242650
TobyRoberts1 <163478842+TobyRoberts1@users.noreply.github.com>
2024-08-13 17:51:49 +1200

    done task 1 amost 2.

---
+++ A2/ServerSession.java
@@ -0,0 +1,85 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+class HttpServer
+{
+    public static void main(String args[])
+    {   
+
+        //the port we will run from. 
+        int port = 51234;
+        try{
+            //creates a server socket to listen on the correct port and shows it. 
+            ServerSocket ss = new ServerSocket(port);
+            System.out.println("Server is listening on port " + port);
+
+            //keeps the server running 
+            while(true){
+                //accepts all incoming connections 
+                Socket s = ss.accept();
+                //creates a new writer to write to console
+                PrintWriter write = new PrintWriter(s.getOutputStream(), true);
+
+                //writes it to client
+                System.out.println("Web Server Starting");
+
+
+
+                //create a new httpserver object then should be able to print out the correct thing. 
+                HttpServerSession httpServer = new HttpServerSession(null, s);
+               
+                
+                s.close();
+            }
+
+        }
+        //catches and prints out any errors
+        catch (Exception e){
+            System.err.println(e);
+        }
+    }
+}
+
+
+class HttpServerSession extends Thread {
+    //each session gets a thread
+    private HttpServer httpServer;
+    private Socket s;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public HttpServerSession(HttpServer httpServer, Socket s){
+        //socket the client uses 
+        this.s = s;
+        //reference to the main thread in the server
+        this.httpServer = httpServer;
+    }
+    public void run() { // entry point into the HttpServerSession
+        // InetAddress ia;
+        // try{
+        //     ia = InetAddress.getByName("localhost");
+        // }
+        // catch(UnknownHostException e){
+        //     System.err.println("unknown anme for ip");
+        //     return;
+        // }
+		try {
+			reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			//writer = new PrintWriter(s.getOutputStream(), true);
+
+			
+            // read a line of text from one client and send itto all other clients
+            String in = reader.readLine();
+            //httpServer.sayToAll(this, in);
+            System.out.println(in);
+
+
+			
+		} 
+        catch (Exception e) {
+			System.err.println("Exception: " + e);
+		}
+    }
+
+}
